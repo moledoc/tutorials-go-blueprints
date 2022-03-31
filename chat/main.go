d3cb75b28253bedbd81283f0d491bd24324a3e46
meelis.utt <meelis.utt@gmail.com>
2022-03-31 15:24:05 +0300

    OAuth2 via google

--- chat/main.go
+++ chat/main.go
@@ -10,6 +10,9 @@ import (
 	"sync"
 
 	"github.com/moledoc/tutorials-go-blueprints/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -22,14 +25,28 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	addr := flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
+	// setup gomniauth
+	gomniauth.SetSecurityKey("This is a security phrase (which is not so good maybe)")
+	gomniauth.WithProviders(
+		google.New(os.Getenv("AUTH_ID"), os.Getenv("AUTH_KEY"), "http://localhost:8080/auth/callback/google"),
+		// TODO: facebook.New(os.Getenv("AUTH_ID"), os.Getenv("AUTH_KEY"), "http://localhost:8080/auth/callback/facebook"),
+		// TODO: github.New(os.Getenv("AUTH_ID"), os.Getenv("AUTH_KEY"), "http://localhost:8080/auth/callback/github"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
+	http.Handle("/", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
