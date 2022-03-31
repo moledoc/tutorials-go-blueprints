bec6b3a4a2dddd1a2159288d45611af236235638
meelis.utt <meelis.utt@gmail.com>
2022-03-31 15:24:04 +0300

    tracer format

--- trace/tracer.go
+++ trace/tracer.go
@@ -7,29 +7,29 @@ import (
 
 // Tracer is the interface that describes an object capable of
 // tracing events throughout code.
-
 type Tracer interface {
 	Trace(...interface{})
 }
 
-type nilTracer struct{}
-
 type tracer struct {
 	out io.Writer
 }
-
-func (t *nilTracer) Trace(a ...interface{}) {}
 
 func (t *tracer) Trace(a ...interface{}) {
 	fmt.Fprint(t.out, a...)
 	fmt.Fprintln(t.out)
 }
 
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+// New is a function that creates a Tracer
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
 }
 
-// Off creates a Tracer that will ignore calls to Trace
+// Off is a function creates a Tracer that will ignore calls to Trace
 func Off() Tracer {
 	return &nilTracer{}
 }
